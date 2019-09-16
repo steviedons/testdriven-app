a647b40f124f1806860b47b36044d82d116b6b3c
Stephen Donovan <steviedons@gmail.com>
2019-09-16 17:58:53 +0100

    test setup done

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -5,16 +5,17 @@ class BaseConfig:
     """ Base configuration"""
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'
 
-class DevelopmentConfig:
+class DevelopmentConfig(BaseConfig):
     """ Development Config """
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
-class TestingConfig:
+class TestingConfig(BaseConfig):
     """Testing config"""
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
-class ProductionConfig:
+class ProductionConfig(BaseConfig):
     """Production config"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
