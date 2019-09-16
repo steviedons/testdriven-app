afa40abf5e06bedab58b405bbc284e4baa0cfe34
Stephen Donovan <steviedons@gmail.com>
2019-09-16 14:15:20 +0100

    initial docker setup done before adding postgres

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,12 +1,17 @@
 # services/users/project/__init__.py
 
+import os
 from flask import Flask, jsonify
 
 # instantiate the app
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SEETINGS')
+app.config.from_object(app_settings)
+
+# To check that the correct config has been loaded
+# print(app.config, file=sys.stderr)
 
 @app.route('/users/ping', methods=['get'])
 def ping_pong():
