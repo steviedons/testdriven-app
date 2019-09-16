aab5d717b53bfd06c80c82119e2e32b4161357cd
Stephen Donovan <steviedons@gmail.com>
2019-09-16 15:22:29 +0100

    Added the db container and started it up

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,7 +1,10 @@
 # services/users/project/__init__.py
 
 import os
+import datetime
+
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the app
 app = Flask(__name__)
@@ -13,8 +16,25 @@ app.config.from_object(app_settings)
 # To check that the correct config has been loaded
 # print(app.config, file=sys.stderr)
 
+# instantiate the db
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+# routes
 @app.route('/users/ping', methods=['get'])
 def ping_pong():
     return jsonify({'status': 'success',
                     'message': 'pong!',
                     })
+                    
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,17 +1,20 @@
 # services/users/project/config.py
+import os
 
 class BaseConfig:
     """ Base configuration"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig:
     """ Development Config """
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig:
     """Testing config"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig:
     """Production config"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
