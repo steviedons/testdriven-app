c65c6a80d42319192fb5f038740806844ecdd650
Stephen Donovan <steviedons@gmail.com>
2019-09-16 16:18:22 +0100

    Postgres setup - Database container is working

--- services/users/manage.py
+++ services/users/manage.py
@@ -2,9 +2,16 @@
 
 from flask.cli import FlaskGroup
 
-from project import app
+from project import app, db
 
 cli = FlaskGroup(app)
+
+# The command name is changed and as actually recreate-db not with the underscore
+@cli.command()
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 if __name__ == '__main__':
     cli()
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -10,7 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # set config
-app_settings = os.getenv('APP_SEETINGS')
+app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
 # To check that the correct config has been loaded
@@ -37,4 +37,3 @@ def ping_pong():
     return jsonify({'status': 'success',
                     'message': 'pong!',
                     })
-                    
