67e325b0abe2dfa87db5291db24747a501bd28c7
Stephen Donovan <steviedons@gmail.com>
2019-09-17 14:16:02 +0100

    blueprint added

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,13 +1,14 @@
 # services/users/manage.py
-
 import unittest
 
 from flask.cli import FlaskGroup
-from project import app, db
 
-cli = FlaskGroup(app)
+from project.api import create_app, db
+from project.api.models import User
 
-# The command name is changed and as actually recreate-db not with the underscore
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
+
 @cli.command()
 def recreate_db():
     db.drop_all()
